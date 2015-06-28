cf28ffe6cc5ddd24912e1bdc4a89a026c94d9c48
Julie Swift <swiftj2@tcnj.edu>
2015-06-28 02:32:34 -0400

    update cfg files

---
+++ proj_anc/model/cfg/master_cfg_nrefls6_rmax1_mu001_fltord150.m
@@ -0,0 +1,10 @@
+
+ui_cfg_1;                                                           % Reads undesired input data into array ui_data
+di_cfg_1;
+mic_cfg_1;
+ls_cfg_1;                                                           % config file #1 for loudspeaker, sets array coeffs
+hs_cfg_1;                                                           % config file #1 for headphone, sets array coeffs
+ra_cfg_nrefls6_rmax1;                                                   % config file #1 to set values that are fed into room_acoustics_to_coeffs
+canceller_cfg_mu001_fltord150;
+
+
